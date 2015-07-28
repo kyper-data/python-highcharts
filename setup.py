399a9ad86677ba335b66e63d674241cd66dca6d2
Kyle Solan <kyle.solan@kyperdata.com>
2015-07-28 01:18:53 +0000

    added setup.py and modified ipynb to make it operational

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+ns = {}
+with open(os.path.join(here, 'highcharts', 'version.py')) as f:
+   exec(f.read(), {}, ns)
+
+setup(
+    name='python-highcharts',
+    version=ns['__version__'],
+    author='Kyper Developers',
+    author_email='developers@kyperdata.com',
+    packages=find_packages(),
+    url='https://git.kyper.co/kyper/kyper-data',
+    description='Python Highcharts wrapper',
+    install_requires=[
+        "Jinja2"
+    ],
+    classifiers         = [
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ]
+)
